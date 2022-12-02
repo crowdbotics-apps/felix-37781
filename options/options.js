581d770a6ffbc5dd40478b1c4b5d5b73ef263843
austin.steil@atcworksolutions.com <austin.steil@atcworksolutions.com>
2022-12-02 16:33:29 -0600

    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,10 @@
-import { StyleSheet } from "react-native";
-
 export const globalOptions = {
   name: "felix_37781",
   url: "https://felix_37781.botics.co",
   api: "https://felix_37781.botics.co/api/v1"
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     "copy": "Routes available!"
   }
-}
+};
