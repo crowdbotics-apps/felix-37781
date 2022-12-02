a4950b6c642c3540de7acdfda53c1dccbe23b131
Crowdbotics <team@crowdbotics.com>
2022-12-02 22:55:18 +0000

    Update options

--- options/options.js
+++ options/options.js
@@ -1,10 +1,22 @@
+const styles = StyleSheet.create({
+  heading: {
+    height: 60,
+    backgroundColor: "#333333",
+    padding: 20,
+    alignItems: "flex-end",
+    flexDirection: "row",
+    justifyContent: "center"
+  }
+})
 export const globalOptions = {
   name: "felix_37781",
   url: "https://felix_37781.botics.co",
   api: "https://felix_37781.botics.co/api/v1"
-};
+}
+
 export const modulesOptions = {
   "@modules/app-menu": {
-    "copy": "Routes available!"
-  }
-};
+    copy: "Routes available!"
+  },
+  "@modules/basiq-payments": { styles: styles }
+}
